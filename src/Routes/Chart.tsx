5d8897b173e22609709412a3e8c9a795422d3e35
psh9408p <psh9408p@naver.com>
2021-12-10 17:10:06 +0900

    캔들차트데이터변환

--- src/Routes/Chart.tsx
+++ src/Routes/Chart.tsx
@@ -20,6 +20,7 @@ export default function Chart({ coinId }: ChartProps) {
   const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistroy(coinId)
   );
+  console.log(data);
   return (
     <div>
       {isLoading ? (
@@ -29,88 +30,28 @@ export default function Chart({ coinId }: ChartProps) {
           type="candlestick"
           series={[
             {
-              data: [
-                {
-                  x: new Date(1538778600000),
-                  y: [6629.81, 6650.5, 6623.04, 6633.33],
-                },
-                {
-                  x: new Date(1538780400000),
-                  y: [6632.01, 6643.59, 6620, 6630.11],
-                },
-                {
-                  x: new Date(1538782200000),
-                  y: [6630.71, 6648.95, 6623.34, 6635.65],
-                },
-                {
-                  x: new Date(1538784000000),
-                  y: [6635.65, 6651, 6629.67, 6638.24],
-                },
-                {
-                  x: new Date(1538785800000),
-                  y: [6638.24, 6640, 6620, 6624.47],
-                },
-                {
-                  x: new Date(1538787600000),
-                  y: [6624.53, 6636.03, 6621.68, 6624.31],
-                },
-                {
-                  x: new Date(1538789400000),
-                  y: [6624.61, 6632.2, 6617, 6626.02],
-                },
-                {
-                  x: new Date(1538791200000),
-                  y: [6627, 6627.62, 6584.22, 6603.02],
-                },
-                {
-                  x: new Date(1538793000000),
-                  y: [6605, 6608.03, 6598.95, 6604.01],
-                },
-                {
-                  x: new Date(1538794800000),
-                  y: [6604.5, 6614.4, 6602.26, 6608.02],
-                },
-                {
-                  x: new Date(1538796600000),
-                  y: [6608.02, 6610.68, 6601.99, 6608.91],
-                },
-                {
-                  x: new Date(1538798400000),
-                  y: [6608.91, 6618.99, 6608.01, 6612],
-                },
-                {
-                  x: new Date(1538800200000),
-                  y: [6612, 6615.13, 6605.09, 6612],
-                },
-                {
-                  x: new Date(1538802000000),
-                  y: [6612, 6624.12, 6608.43, 6622.95],
-                },
-                {
-                  x: new Date(1538803800000),
-                  y: [6623.91, 6623.91, 6615, 6615.67],
-                },
-                {
-                  x: new Date(1538805600000),
-                  y: [6618.69, 6618.74, 6610, 6610.4],
-                },
-                {
-                  x: new Date(1538807400000),
-                  y: [6611, 6622.78, 6610.4, 6614.9],
-                },
-                {
-                  x: new Date(1538809200000),
-                  y: [6614.9, 6626.2, 6613.33, 6623.45],
-                },
-              ],
+              data: data?.map((item) => {
+                const x = new Date(item?.time_open);
+                const y = [
+                  item.open,
+                  item.high,
+                  item.low,
+                  item.close,
+                  item.volume,
+                ];
+                return { x, y };
+              }),
             },
           ]}
           options={{
             theme: {
               mode: "dark",
             },
+            xaxis: {
+              type: "datetime",
+            },
             chart: {
-              height: 350,
+              height: 500,
               width: 500,
               toolbar: {
                 show: false,
